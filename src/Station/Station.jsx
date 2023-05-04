72423fca5560ed6d03f3943b5a39db02385c65dc
Daniel-Ibarrola <dis96051@gmail.com>
2023-05-04 11:30:22 -0600

    Integration tests for Station in progress

--- src/Station/Station.jsx
+++ src/Station/Station.jsx
@@ -1,7 +1,26 @@
+import * as React from "react";
+import { useParams } from "react-router-dom";
+import station_names from "../shared/station_names.js";
 
 
 const Station = () => {
-    return <h1>Station</h1>;
+    const { stationName } = useParams();
+    let prettyName = "";
+    if (Object.hasOwn(station_names, stationName)){
+        prettyName = station_names[stationName];
+    }
+
+    return (
+        <div>
+            {prettyName !== ""
+                ?
+                <h1>{prettyName}</h1>
+                :
+                <h1>Estación Invalida</h1>
+            }
+
+        </div>
+    );
 };
 
 export { Station };
--- src/Station/test.jsx
+++ src/Station/test.jsx
@@ -1,9 +1,60 @@
-import { describe, it, expect } from "vitest";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import {render, screen, waitFor} from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
 import { Station } from "./Station.jsx";
 
+vi.mock("axios");
+vi.mock("react-router-dom");
+
+const voltages = [
+    {
+        "date": "2023-03-31T00:00:00",
+        "battery": 100.50,
+        "panel": 200.80,
+    },
+    {
+        "date": "2023-03-31T12:00:00",
+        "battery": 50.50,
+        "panel": 150.80,
+    }
+]
+
+const reports = [
+    {
+        date: "2023-03-31",
+        reports: 2,
+    }
+]
 
 describe("Station", () => {
-    it("Temporary test. Replace later", () => {
-        expect(true).toBeTruthy();
+    it("Displays error in case of invalid station parameter", () => {
+        useParams.mockImplementationOnce(() => "EstacionDesconocida");
+        render(<Station />);
+        expect(screen.queryByText("Estación Invalida")).toBeInTheDocument();
     });
+
+    it("Fails fetching data",  async () => {
+       const promise = Promise.reject();
+       axios.get.mockImplementationOnce(() => promise);
+       expect(screen.queryByText(/Cargando/)).toBeInTheDocument();
+
+        try {
+            await waitFor(async () => await promise);
+        } catch (error) {
+            expect(screen.queryByText(/Cargando/)).toBeNull();
+            expect(screen.queryByText(/No se pudo cargar/)).toBeInTheDocument();
+        }
+    });
+
+    it("Fetch data and displays statistics", async () => {
+        const promise = Promise.resolve({
+           data: {
+               reports: reports,
+               voltages: voltages,
+           }
+        });
+
+    });
+
 });
