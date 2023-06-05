f56577165753360753a8119fbf2be108bb622422
Daniel-Ibarrola <dis96051@gmail.com>
2023-06-05 08:22:32 -0600

    Login and Register components layout

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,7 +1,70 @@
-
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 const Login = () => {
-    return <h2>Login</h2>
+
+    const handleEmailChange = (event) => {
+        console.log(event.target.value);
+    }
+
+    const handlePasswordChange = (event) => {
+        console.log(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <Container>
+            <Row className="justify-content-center">
+                <Col md={{span: 6}}>
+                        <Card className="login-card">
+                            <Card.Title className="login-title">Iniciar Sesión</Card.Title>
+                            <hr />
+                            <Card.Body className="login-body">
+                                    <Form onSubmit={handleSubmit}>
+                                        <Form.Group>
+                                            <Form.Label htmlFor="email">Correo</Form.Label>
+                                            <Form.Control
+                                                type="email"
+                                                placeholder={"Email"}
+                                                id="email"
+                                                onChange={handleEmailChange}
+                                                required
+                                                className="login-row"
+                                            />
+                                        </Form.Group>
+                                        <Form.Group>
+                                            <Form.Label htmlFor="password">Contraseña</Form.Label>
+                                            <Form.Control
+                                                type="password"
+                                                placeholder="Contraseña"
+                                                id="password"
+                                                onChange={handlePasswordChange}
+                                                required
+                                                className="login-row"
+                                            />
+                                        </Form.Group>
+                                        <div className="d-grid gap-2 login-button">
+                                            <Button type="submit">Iniciar sesión</Button>
+                                        </div>
+                                    </Form>
+                                    <Card.Link>
+                                        <Link to="/register">Crear una cuenta</Link>
+                                    </Card.Link>
+                            </Card.Body>
+                        </Card>
+                </Col>
+            </Row>
+        </Container>
+    )
 };
 
 
--- src/Auth/Register.jsx
+++ src/Auth/Register.jsx
@@ -1,7 +1,71 @@
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./style.css"
 
 const Register = () => {
-    return <h2>Register</h2>
+    // TODO: add form validation
+
+    const handleEmailChange = (event) => {
+        console.log(event.target.value);
+    }
+
+    const handlePassswordChange = (event) => {
+        console.log(event.target.value);
+    }
+
+    const handleSumbit = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <Container>
+            <Row className="justify-content-center">
+                <Col md={{span: 6}}>
+                    <Card className="login-card">
+                        <Card.Title className="login-title">Registrarse</Card.Title>
+                        <hr />
+                        <Card.Body className="login-body">
+                            <Form onSubmit={handleSumbit}>
+                                <Form.Group>
+                                    <Form.Label htmlFor="email">Correo</Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        placeholder={"Email"}
+                                        id="email"
+                                        onChange={handleEmailChange}
+                                        required
+                                        className="login-row"
+                                    />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label htmlFor="password">Contraseña</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="Contraseña"
+                                        id="password"
+                                        onChange={handlePassswordChange}
+                                        required
+                                        className="login-row"
+                                    />
+                                    <Form.Text muted className="password-text">
+                                        Su contraseña debe tener entre 8-20 caracteres, contener
+                                        letras y numeros, y no debe contener espacios.
+                                    </Form.Text>
+                                </Form.Group>
+                                <div className="d-grid gap-2 login-button">
+                                    <Button type="submit">Crear cuenta</Button>
+                                </div>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    )
 };
 
 export { Register };
